1456b2bfe5e50996137e5683a832888cf5d3864d
Yash Gupta <yashguptaab99@gmail.com>
2022-07-30 17:50:15 +0530

    refactor: repo structure

--- src/exercise.ts
+++ src/exercise.ts
@@ -1,58 +1,71 @@
 import axios from 'axios';
-import { Web3Wrapper, TxData, SupportedProvider } from "@0x/web3-wrapper";
-import { GetSwapQuoteResponse, ZeroExSwapAPIParams, ERC20TokenContract } from "./misc";
-import { getContractAddressesForChainOrThrow, ChainId } from "@0x/contract-addresses";
+import { Web3Wrapper, TxData, SupportedProvider } from '@0x/web3-wrapper';
+import {
+  GetSwapQuoteResponse,
+  ZeroExSwapAPIParams,
+  ERC20TokenContract,
+} from './misc';
+import {
+  getContractAddressesForChainOrThrow,
+  ChainId,
+} from '@0x/contract-addresses';
 import { BigNumber } from '@0x/utils';
 import { Web3ProviderEngine } from '@0x/subproviders';
 
-const zeroExDeployedAddresses = getContractAddressesForChainOrThrow(ChainId.Kovan);
+const zeroExDeployedAddresses = getContractAddressesForChainOrThrow(
+  ChainId.Kovan
+);
 
+async function introToERC20TokenContract(
+  web3Provider: Web3ProviderEngine
+): Promise<void> {
+  // A quick example of ERC20TokenContract
 
-async function introToERC20TokenContract(web3Provider: Web3ProviderEngine): Promise<void> {
-    // A quick example of ERC20TokenContract
+  // Initializing a new instance of ERC20TokenContract
+  const tokenAddress = '0x48178164eB4769BB919414Adc980b659a634703E'; // Address of fake DAI token
+  const tokenContract: ERC20TokenContract = new ERC20TokenContract(
+    tokenAddress,
+    web3Provider
+  );
 
-    // Initializing a new instance of ERC20TokenContract
-    const tokenAddress = '0x48178164eB4769BB919414Adc980b659a634703E' // Address of fake DAI token
-    const tokenContract: ERC20TokenContract = new ERC20TokenContract(tokenAddress, web3Provider);
+  // Reading a value on the blockchain does NOT require a transaction.
+  const name = await tokenContract.name().callAsync();
+  const decimals = await tokenContract.decimals().callAsync();
+  const balance = await tokenContract.balanceOf('0xSomeAddress').callAsync();
 
-    // Reading a value on the blockchain does NOT require a transaction.
-    const name = await tokenContract.name().callAsync()
-    const decimals = await tokenContract.decimals().callAsync()
-    const balance = await tokenContract.balanceOf('0xSomeAddress').callAsync()
+  console.log(name); // DAI
+  console.log(decimals); // 18
+  console.log(balance); // 100000000000000000000
 
-    console.log(name) // DAI
-    console.log(decimals) // 18
-    console.log(balance) // 100000000000000000000
-
-    // Writing a value on the blockchain 
-    await tokenContract.transfer(
-        '0xSomeOtherAddress',
-        new BigNumber(100000000000000000000),
-    ).awaitTransactionSuccessAsync({
-        from: '0xMyAddress',
+  // Writing a value on the blockchain
+  await tokenContract
+    .transfer('0xSomeOtherAddress', new BigNumber(100000000000000000000))
+    .awaitTransactionSuccessAsync({
+      from: '0xMyAddress',
     });
 }
-
-
 
 /**
  * Converts a humanly-readable number (that may contain decimals, example: 133.232) into a big integer.
  * Why do we need this: Ethereum can only only store integer values, so, in order to generate a number
  * that can be diplayed to users (in a UI), you need to store that number as a big integer + the number of
  * decimal places.
- * 
+ *
  * Example:
  * (USDC has 6 decimals, DAI has 18 decimals)
- * 
+ *
  * - convertValueFromHumanToEthereum(usdcToken, 5) returns 5000000
  * - convertValueFromHumanToEthereum(daiToken, 20.5) returns 20500000000000000000
- * 
+ *
  * @param tokenWrapper an instance of the ERC20 token wrapper
  * @param unitAmount a number representing the human-readable number
  * @returns a big integer that can be used to interact with Ethereum
  */
-async function convertValueFromHumanToEthereum(tokenWrapper: ERC20TokenContract, unitAmount: number): Promise<BigNumber> {
-    throw new Error('Implement me!')
+async function convertValueFromHumanToEthereum(
+  tokenWrapper: ERC20TokenContract,
+  unitAmount: number
+): Promise<BigNumber> {
+  throw new Error('Implement me!');
 }
 
 /**
@@ -64,35 +77,34 @@ async function convertValueFromHumanToEthereum(tokenWrapper: ERC20TokenContract,
  * @param client the Web3Wrapper client
  */
 export async function performSwapAsync(
-    buyTokenWrapper: ERC20TokenContract,
-    sellTokenWrapper: ERC20TokenContract,
-    amountToSellUnitAmount: number,
-    fromAddress: string,
-    provider: SupportedProvider,
+  buyTokenWrapper: ERC20TokenContract,
+  sellTokenWrapper: ERC20TokenContract,
+  amountToSellUnitAmount: number,
+  fromAddress: string,
+  provider: SupportedProvider
 ): Promise<void> {
+  // Step #1) Does the user have enough balance?
+  // Convert the unit amount into base unit amount (bigint). For this to happen you need the number of decimals the token.
 
-    // Step #1) Does the user have enough balance?
-    // Convert the unit amount into base unit amount (bigint). For this to happen you need the number of decimals the token.
+  // Step #2) Does the 0x ERC20 Proxy have permission to withdraw funds from the exchange?
+  // In order to allow the 0x smart contracts to trade with your funds, you need to set an allowance for zeroExDeployedAddresses.erc20Proxy.
+  // This can be done using the `approve` function.
 
-    // Step #2) Does the 0x ERC20 Proxy have permission to withdraw funds from the exchange?
-        // In order to allow the 0x smart contracts to trade with your funds, you need to set an allowance for zeroExDeployedAddresses.erc20Proxy.
-        // This can be done using the `approve` function.
-        
-    // Step #3) Make a request to the 0x API swap endpoint: https://0x.org/docs/guides/swap-tokens-with-0x-api#swap-eth-for-1-dai
-    // You can use the line below as guidance. In the example, the variable TxData contains the deserialized JSON response from the API.
-    // const url = `https://kovan.api.0x.org/swap/v0/quote`;
-    // const params: ZeroExSwapAPIParams = {
-    // }
-    // const httpResponse = await axios.get<GetSwapQuoteResponse>(url, { params })
-    // const jsonResponse = httpResponse.data; // This is the JSON response from the API.
-    // const txData: TxData = {
-    // };
-    // console.log(`Ethereum transaction generated by the 0x API: 👇`);
-    // console.log(txData);
-    
-    // console.log(`Orders used to perform the swap 👇`);
-    // console.log(httpResponse.data.orders);
+  // Step #3) Make a request to the 0x API swap endpoint: https://0x.org/docs/guides/swap-tokens-with-0x-api#swap-eth-for-1-dai
+  // You can use the line below as guidance. In the example, the variable TxData contains the deserialized JSON response from the API.
+  // const url = `https://kovan.api.0x.org/swap/v0/quote`;
+  // const params: ZeroExSwapAPIParams = {
+  // }
+  // const httpResponse = await axios.get<GetSwapQuoteResponse>(url, { params })
+  // const jsonResponse = httpResponse.data; // This is the JSON response from the API.
+  // const txData: TxData = {
+  // };
+  // console.log(`Ethereum transaction generated by the 0x API: 👇`);
+  // console.log(txData);
 
-    // Step #4) You can `client.sendTransactionAsync()` to send a Ethereum transaction.
-    alert('Implement me!')
+  // console.log(`Orders used to perform the swap 👇`);
+  // console.log(httpResponse.data.orders);
+
+  // Step #4) You can `client.sendTransactionAsync()` to send a Ethereum transaction.
+  alert('Implement me!');
 }
